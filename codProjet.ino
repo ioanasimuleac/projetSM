c35eb82af24d7ff77eb6be4dcc82d37727a03767
Ioana Simuleac <ioana.simuleac@gmail.com>
2019-01-16 19:10:11 +0200

    le marche avec un seul LED: allumer/eteindre

--- codProjet.ino
+++ codProjet.ino
@@ -13,19 +13,23 @@ SoftwareSerial BTserial(2, 3); // RX | TX
 //
  
 char c = ' ';
- 
+String cuvant ;
+String on = "on";
+String off = "off";
 // BTconnected will = false when not connected and true when connected
 boolean BTconnected = false;
  
 // connect the STATE pin to Arduino pin D4
 const byte BTpin = 4;
+int led = 13;
  
  
 void setup() 
 {
     // set the BTpin for input
     pinMode(BTpin, INPUT);   
- 
+    pinMode(led,OUTPUT);
+    
     // start serial communication with the serial monitor on the host computer
     Serial.begin(9600);
     Serial.println("Arduino is ready");
@@ -36,30 +40,46 @@ void setup()
     {
       if ( digitalRead(BTpin)==HIGH)  { BTconnected = true;};
     }
- 
     Serial.println("HC-05 is now connected");
-    Serial.println("");
  
     // Start serial communication with the bluetooth module
     // HC-05 default serial speed for communication mode is 9600 but can be different
-    BTserial.begin(9600);  
+    BTserial.begin(9600); 
+    BTserial.write('x'); 
 }
  
 void loop()
 {
  
     // Keep reading from the HC-05 and send to Arduino Serial Monitor
-    if (BTserial.available())
-    {  
+    if (BTserial.available()){     
         c = BTserial.read();
         Serial.write(c);
+        Serial.println("");
+        switch(c){
+          case 'a':
+            digitalWrite(led,HIGH);
+            //delay(1000);
+            BTserial.write('A');
+            BTserial.println("");
+            Serial.println("led on");
+            break;
+
+          case 'i':
+            digitalWrite(led,LOW);
+            //delay(1000);
+            BTserial.write('D');
+            BTserial.println("");
+            Serial.println("led off");
+            break;
+          }
     }
  
-    // Keep reading from Arduino Serial Monitor input field and send to HC-05
-    if (Serial.available())
-    {
-        c =  Serial.read();
-        BTserial.write(c);  
-    }
+//    // Keep reading from Arduino Serial Monitor input field and send to HC-05
+//    if (Serial.available())
+//    {
+//        c =  Serial.read();
+//        BTserial.write(c);  
+//    }
  
 }
